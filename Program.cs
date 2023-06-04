ad803a0bc281c715aa68962271cca75a7ade2822
WhatIsFun <mohammed110051@gmail.com>
2023-06-04 09:38:54 +0400

    Change

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace git_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
